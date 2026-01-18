852f988bdca482b29b83d9402622f13af65298e2
Natalie Daubner <ncdaubner@gmail.com>
2026-01-17 22:12:48 -0500

    Added separate player spawn script
    Allows player_spawn to be placed anywhere within a scene, which provides a place for the player to spawn and function without manually adding player scene

--- 00_Globals/GlobalPlayerManager.gd
+++ 00_Globals/GlobalPlayerManager.gd
@@ -3,6 +3,7 @@ extends Node
 const PLAYER = preload("res://player.tscn")
 
 var player : Player
+var player_spawned : bool = false
 
 func _ready() -> void:
 	add_player_instance()
@@ -10,4 +11,7 @@ func _ready() -> void:
 func add_player_instance() -> void:
 	player = PLAYER.instantiate()
 	add_child(player)
+	pass
+func set_player_position(_new_pos : Vector2) -> void:
+	player.global_position = _new_pos
 	pass
