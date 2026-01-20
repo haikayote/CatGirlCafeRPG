052a228521bd7b23cb87986fdef1285e18fe2d11
Natalie Daubner <ncdaubner@gmail.com>
2026-01-20 16:10:02 -0500

    Connected levels w/ smooth transition
    Connected cafe level with bedroom scene, adding a fade scene transition

--- Levels/scripts/level.gd
+++ Levels/scripts/level.gd
@@ -5,3 +5,9 @@ class_name Level extends Node2D
 func _ready() -> void:
 	self.y_sort_enabled = true
 	GlobalPlayerManager.set_as_parent(self)
+	LevelManager.level_load_started.connect(_free_level)
+
+func _free_level() -> void:
+	GlobalPlayerManager.unparent_player(self)
+	queue_free() #player won't be part of the previous level when level is removed from the game
+	pass
