4137b91554ff086697241087e0fd4413e0027061
Natalie Daubner <ncdaubner@gmail.com>
2026-01-24 22:48:50 -0500

    Setting up NPC Resource file
    Light day today, setting up more files. Back at it again tomorrow with NPC scene setup!

--- npc/scripts/npc_resource.gd
+++ npc/scripts/npc_resource.gd
@@ -1,6 +1,9 @@
-class_name NPC extends CharacterBody2D
+class_name NPCResource extends Resource
 
-
+@export var npc_name : String = ""
+@export var sprite : Texture
+@export var portrait : Texture
+@export var dialog_audio_pitch : String
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
