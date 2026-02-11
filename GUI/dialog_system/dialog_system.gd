de76c935b3cf58d3202ec9f9f764e1f29b980652
Natalie Daubner <ncdaubner@gmail.com>
2026-02-11 12:56:40 -0500

    Modified dialog audio pitch + cleanup
    Had to remove NPC #2 for cleanup, created a dialog system for NPC #1, with a dialog pitch mod for both player and NPC. Added a blink animation to script.

--- GUI/dialog_system/dialog_system.gd
+++ GUI/dialog_system/dialog_system.gd
@@ -3,8 +3,14 @@
 class_name DialogSystemNode extends CanvasLayer
 
 signal finished
+signal letter_added(letter:String)
 
 var is_active : bool = false
+var text_in_progress : bool = false
+
+var text_speed : float = 0.02
+var text_length : int = 0
+var plain_text : String 
 
 var dialog_items : Array[DialogItem]
 var dialog_item_index : int = 0 # keeps track of dialog progression
@@ -12,9 +18,12 @@ var dialog_item_index : int = 0 # keeps track of dialog progression
 @onready var dialog_ui: Control = $DialogUI
 @onready var content: RichTextLabel = $DialogUI/PanelContainer/RichTextLabel
 @onready var name_label: RichTextLabel = $DialogUI/NameLabel
-@onready var portrait_sprite: Sprite2D = $DialogUI/PortraitSprite
+@onready var portrait_sprite: DialogPortrait = $DialogUI/PortraitSprite
 @onready var dialog_progress: PanelContainer = $DialogUI/DialogProgress
 @onready var dialog_progress_label: RichTextLabel = $DialogUI/DialogProgress/RichTextLabel
+@onready var timer: Timer = $DialogUI/Timer
+@onready var audio_stream_player: AudioStreamPlayer = $DialogUI/AudioStreamPlayer
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -23,6 +32,7 @@ func _ready() -> void:
 			get_parent().remove_child(self)
 			return
 		return
+	timer.timeout.connect(_on_timer_timeout)
 	hide_dialog()
 	pass # Replace with function body.
 
@@ -35,6 +45,8 @@ func _unhandled_input(event: InputEvent) -> void:
 			event.is_action_pressed("ui_accept") or 
 			event.is_action_pressed("attack")
 	):
+		if text_in_progress == true:
+			return
 		dialog_item_index += 1 #adds to index, furthering dialog
 		if dialog_item_index < dialog_items.size():
 			start_dialog()
@@ -63,9 +75,16 @@ func hide_dialog() -> void:
 	pass
 	
 func start_dialog() -> void:
-	show_dialog_button_indicator(true)
+	show_dialog_button_indicator(false)
 	var _d : DialogItem = dialog_items[dialog_item_index]
 	set_dialog_data(_d)
+	
+	content.visible_characters = 0
+	text_length = content.get_total_character_count()
+	plain_text = content.get_parsed_text()
+	text_in_progress = true
+	start_timer()
+	
 	pass
 		
 func set_dialog_data(_d : DialogItem) -> void:
@@ -73,6 +92,7 @@ func set_dialog_data(_d : DialogItem) -> void:
 		content.text = _d.text
 	name_label.text = _d.npc_info.npc_name
 	portrait_sprite.texture = _d.npc_info.portrait
+	portrait_sprite.audio_pitch_base = _d.npc_info.dialog_audio_pitch
 	pass
 func show_dialog_button_indicator(_is_visible : bool) -> void:
 	dialog_progress.visible = _is_visible
@@ -82,3 +102,20 @@ func show_dialog_button_indicator(_is_visible : bool) -> void:
 		dialog_progress_label.text = "END"
 	pass
 	
+func start_timer() -> void:
+	timer.wait_time = text_speed
+	#manipulate wait_time
+	timer.start()#everytime timer runs out, add another letter
+	pass
+	
+func _on_timer_timeout() -> void:
+	#add a character to visible characters
+	print("_on_timer_timeout()")
+	content.visible_characters += 1
+	if content.visible_characters <= text_length:
+		letter_added.emit(plain_text[content.visible_characters-1])
+		start_timer()
+	else:
+		show_dialog_button_indicator(true)
+		text_in_progress = false
+	pass
--- npc/scripts/npc_resource.gd
+++ npc/scripts/npc_resource.gd
@@ -3,7 +3,7 @@ class_name NPCResource extends Resource
 @export var npc_name : String = ""
 @export var sprite : Texture
 @export var portrait : Texture
-@export var dialog_audio_pitch : String
+@export var dialog_audio_pitch : float = 1.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
