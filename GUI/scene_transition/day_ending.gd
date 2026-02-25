fb3747880e3c21f0df156b62afa0c8bb4de7da75
Natalie Daubner <ncdaubner@gmail.com>
2026-02-24 23:11:42 -0500

    Day 1 Ending + NPCs Day 2
    Working on transitions and NPCs for Day 2. May consolidate into an array of packed scenes so that it saves a bit of space from creating multiple scenes for each day.

---
+++ GUI/scene_transition/day_ending.gd
@@ -0,0 +1,18 @@
+class_name DayEnding extends CanvasLayer
+
+@onready var button = $VBoxContainer/Button
+
+
+func _ready() -> void:
+	button.pressed.connect(_on_button_pressed)
+#if button is pressed:
+# scene = day 2 bedroom w new dialogs
+# remove other scene
+# day 2 bedroom scene should have transition to day 2 lobby w / day 2 npcs
+func _on_button_pressed() -> void:
+	var level_scene = load("res://Levels/Area1/02.tscn")
+	var level_instance = level_scene.instantiate()
+	var player_spawn = level_instance.get_node("PlayerSpawn")
+	LevelManager.load_new_level(("res://Levels/Area1/02.tscn"), ("res://Levels/Area1/02.tscn"),Vector2.ZERO)
+	GlobalPlayerManager.set_player_position(player_spawn.global_position)
+	pass
